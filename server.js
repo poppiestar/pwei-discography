346c410d52b4ed32d3fa3eccceebc3d756358142
Drew Miller <drew@onpatrol.co>
2015-03-26 23:32:36 +0000

    Added basic release page

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var port = process.env.PORT || 3000;
 var routes = require('./lib/routes');
 
 app.get('/releases', routes.releases);
+app.get('/release/:id', routes.release);
 
 app.listen(port, function () {
     console.log('Server started on port: ', port);
